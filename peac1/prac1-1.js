5f484135afe9da119dd87663459f209c78e00a20
akira <a.oono.sys23@morijyobi.sc.jp>
2024-03-19 14:55:19 +0900

    練習問題

---
+++ peac1/prac1-1.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let height = window.prompt("ユーザが身長を入力するダイヤログ");
+
+
+if(height == false){
+    window.alert("入力がないと判定できません");
+}
+else if(height < 100){
+    if(height < 90){
+        window.alert("乗車できません");
+    }else{
+        window.alert("付き添いありで乗車可能です");
+    }
+}else if(height >= 100){
+    
+    let ticket = window.confirm("プレミアムチケットを持っていますか?");
+    
+    if(ticket == true){
+        window.alert("プレミアムシートに乗車可能です");
+    }else{
+        window.alert("通常のシートに乗車可能です");
+    }
+
+}else{
+    window.alert("半角数字で入力してください");
+}
---
+++ peac1/prac1-2.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let station = window.prompt("目的の駅を入力してください\n1 : A駅\n2 : B駅\n3 : C駅");
+
+switch(station){
+    case 1:
+        window.alert("快速電車が停まります");
+        break;
+    case 2:
+        window.alert("快速,急行電車が停まります");
+        break;
+    case 3:
+        window.alert("特急電車が停まります")
+}
